f1de126d66489ce89674c1e41ad9952400fd3d04
Chris <chrismbusse@gmail.com>
2020-08-20 21:47:49 -0400

    added hello capabilities to gordon

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ client.on('ready', () => console.log('Ready!'));
 client.on('message', (msg) => {
     if (msg.author.bot) return;
 
-    if (msg.content.startsWith('!ping')) {
-        msg.channel.send('!gnip');
+    if (msg.content.match(/(hi|hello|hey|yo|sup|what's up|whats up) gordon/i)) {
+        msg.channel.send('Hello ' + msg.author.username);
+    }
+
+    if (msg.content.match(/^ping$/i)) {
+        msg.channel.send('pong');
     }
 });
 
