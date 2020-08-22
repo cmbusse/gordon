60aeaf426dd24a057ac85e4f8d8f80351b5de59c
Chris <chrismbusse@gmail.com>
2020-08-21 20:26:46 -0400

    Adding ability to log messages to Splunk, and implementing testing server capabilities.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Client } = require('discord.js');
-const { token } = require('./settings');
+const axios = require('axios');
+const https = require('https');
+const { token, CAT_API_KEY, DOG_API_KEY, HEC_TOKEN, splunk_url } = require('./settings');
 const client = new Client();
 
 client.on('ready', () => console.log('Ready!'));
@@ -7,13 +9,50 @@ client.on('ready', () => console.log('Ready!'));
 client.on('message', (msg) => {
     if (msg.author.bot) return;
 
-    if (msg.content.match(/(hi|hello|hey|yo|sup|what's up|whats up) gordon/i)) {
-        msg.channel.send('Hello ' + msg.author.username);
+    if (!isTesting(msg)) {
+        if (msg.content.match(/(hi|hello|hey|yo|sup|what's up|whats up) gordon/i)) {
+            msg.channel.send('Hello ' + msg.author.username);
+        }
+    
+        if (msg.content.match(/^ping$/i)) {
+            msg.channel.send('pong');
+        }
     }
 
-    if (msg.content.match(/^ping$/i)) {
-        msg.channel.send('pong');
+    if (isTesting(msg)) {
+        if (msg.content.match(/^test$/i)) {
+            msg.channel.send('Receiving transmission.');
+            //let event = 'Set a course.';
+            //logToSplunk(event);
+        }
     }
 });
+
+function isTesting(msg) {
+    if (msg.channel.id === '744625770642800713') {
+        return true;
+    }
+}
+
+function logToSplunk(event) {
+    const agent = new https.Agent({  
+        rejectUnauthorized: false
+      });
+    const config = {
+        headers: { 'Authorization': `Splunk ${HEC_TOKEN}`},
+        httpsAgent: agent
+    }
+
+    params = {
+        event: event
+    }
+
+    axios.post(splunk_url, params, config)
+    .then((response) => {
+        console.log(response.data);
+    }, (error) => {
+        console.log(error);
+    });
+}
 
 client.login(token);
