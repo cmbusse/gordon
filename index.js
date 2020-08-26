c7c6bb4d710ea22409411b1bced6845656623939
Chris <chrismbusse@gmail.com>
2020-08-26 17:36:54 -0400

    Adding catification

--- index.js
+++ index.js
@@ -21,12 +21,16 @@ client.on('message', (msg) => {
         if(msg.content.match(/^woof!$/i)) {
             getADog(msg);
         }
+
+        if(msg.content.match(/^meow!$/i)) {
+            getACat(msg);
+        }
     }
     
     if (isTesting(msg) === true) {
-        if (msg.content.match(/^woof!$/i)) {
+        if (msg.content.match(/^test$/i)) {
             //msg.channel.send('Receiving transmission.');
-            getADog(msg);
+            getACat(msg);
         }
     }
 
@@ -65,7 +69,6 @@ function logToSplunk(event) {
 }
 
 function getADog(msg) {
-    // might need to stringify the message
     var url = 'https://api.thedogapi.com/v1/images/search';
     const config = {
         headers: { 'X-API-KEY': DOG_API_KEY }
@@ -81,6 +84,31 @@ function getADog(msg) {
             .setTitle('Here, have a dog!')
             .setImage(image)
             .setFooter('BORK!');
+
+        //msg.channel.send(`success ${JSON.stringify(response.data)}`);
+        //msg.channel.send(`url: ${JSON.stringify(response['data'][0]['url'])}`);
+        msg.channel.send(embed);
+    }, (error) => {
+        msg.channel.send('error ' + error);
+    });
+}
+
+function getACat(msg) {
+    var url = 'https://api.thecatapi.com/v1/images/search';
+    const config = {
+        headers: { 'X-API-KEY': CAT_API_KEY }
+    }
+    axios.get(url, config)
+    .then((response) => {
+        //import Discord from 'discord.js';
+        
+        var image = response['data'][0]['url'];
+
+        const embed = new Discord.MessageEmbed()
+            .setColor('#36393f')
+            .setTitle('Here, have a cat!')
+            .setImage(image)
+            .setFooter('MEOW!');
 
         //msg.channel.send(`success ${JSON.stringify(response.data)}`);
         //msg.channel.send(`url: ${JSON.stringify(response['data'][0]['url'])}`);
